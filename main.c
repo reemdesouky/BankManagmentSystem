b0afc001da982f396c0fca32cd75e51152f3e13f
reemdesouky <126808249+reemdesouky@users.noreply.github.com>
2023-12-20 20:57:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,87 @@
 #include <stdio.h>
+#include <string.h>
+typedef struct {
+    int day;
+    int month;
+    int year;
+}date;
+typedef struct{
+    int accountnum;
+    char[100] name;
+    int mobilnum;
+    char[50] mail;
+    float balance; 
+    date dateOpened;
+}Accounts;
+void checkLogin (char user[],int pass)
+{
+    
+}
+void load ()
+{
+    char info [200][200]
+    FILE *p=fopen("users.txt"."r");
+}
+void menu ()
+{
+    
+}
+void add()
+{
+    
+}
+void accountDelete()
+{
+    
+}
+void accountModify()
+{
+    
+}
+void accountSearch()
+{
+    
+}
+void withdraw()
+{
+    
+}
+void deposite()
+{
+    
+}
+void transfer()
+{
+    
+}
+void report()
+{
+    
+}
+void print()
+{
+    
+}
+void sortByName()
+{
+    
+}
+void sortByDate()
+{
+    
+}
+void sortByBalance()
+{
+    
+}
+void save()
+{
+    
+}
+void exit()
+{
+    
+}
 void main()
 {
     char user[15];
